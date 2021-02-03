8e70bc65bc1ffe406086f826dfe469a4ebb13e28
mahxmoud <mahmoud.jomaa@yahoo.com>
2021-02-03 17:48:45 +0200

    connected server to database

--- server/index.js
+++ server/index.js
@@ -1,2 +1,24 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+
+
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://Mahxmoud:mahxmoud1234@cluster0.orfcx.mongodb.net/<dbname>?retryWrites=true&w=majority"
+
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
+    .catch((err) => console.log(err.message))
+
+mongoose.set('useFindAndModify', false) 
