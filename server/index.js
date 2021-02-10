6043455f6f4f3ea4ab2303ceb78665fb7909bb5a
mahxmoud <mahmoud.jomaa@yahoo.com>
2021-02-11 00:08:20 +0200

    created auth logic on the server

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import courseRoute from './routes/courses.js';
+import userRoute from './routes/users.js';
 
 const app = express();
 dotenv.config()
@@ -14,7 +15,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 
 app.use('/courses', courseRoute)
-
+app.use('/user', userRoute)
 
 
 
---
+++ server/middleware/auth.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+const secret = 'test';
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decodedData = jwt.verify(token, secret);
+
+        req.userId = decodedData?.id;
+        
+        next();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export default auth;
